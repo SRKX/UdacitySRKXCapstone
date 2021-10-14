5976180848b684d0505f11791e401cb953a8495b
Jeremie Smaga <jeremie@smaga.ch>
2021-10-14 22:04:30 +0800

    Correctly deploys both required contracts

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,19 +1,22 @@
 // migrating the appropriate contracts
-/*
-
-*/
-//var JSUdacityERC721Token = artifacts.require("./JSUdacityERC721Token.sol");
 var SquareVerifier = artifacts.require("./Verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 
 module.exports = function(deployer) {
-  //deployer.deploy(JSUdacityERC721Token, "TEST", "TST");
-  
+ 
+  console.log( "!!!Starting to deploy!!!!");
 
     
   deployer.deploy(SquareVerifier).then( () => {
-    deployer.deploy(SolnSquareVerifier, SquareVerifier.address, "TEST", "TST");
+    return deployer.deploy(SolnSquareVerifier, SquareVerifier.address, "TEST", "TST").then( () => {
+      console.log( "Verifier address: %s", SquareVerifier.address);
+      console.log( "SolnSquareVerifier address: %s", SolnSquareVerifier.address);
+    }).catch( () => {
+      console.log( "Something FAILED");
+    })
+  }).catch(  () => {
+    console.log( "Something FAILED");
   });
   
   
